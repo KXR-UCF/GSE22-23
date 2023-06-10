f66ec5232d426059d3f6604ae9e930fd4810d4d7
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2023-06-09 21:57:24 -0400

    updated servo change

--- servo_nano/reciever.ino
+++ servo_nano/reciever.ino
@@ -1,23 +1,23 @@
 #include "Servo.h"
 
-#define N_PIN 7
-#define E_PIN 8
-#define F_PIN 9
-#define V_PIN 11
+#define N_PIN 11
+#define E_PIN 9
+#define F_PIN 7
+#define V_PIN 8
 
-#define CLOSE 98 // DPF close =98    IREC close= 0
-#define OPEN 35  // DPF open = 35    IREC open = 120                        //Use E_pin / Ethanol for IREC valve
+#define CLOSE 95 // DPF close =98    IREC close= 0
+#define OPEN 45  // DPF open = 40    IREC open = 120                        //Use E_pin / Ethanol for IREC valve
 
 Servo n;
 Servo e;
 Servo f;
 Servo v;
 
-const int SIGNAL_PIN1 = 2;  //  23   ETh
-const int SIGNAL_PIN2 = 3;  //  24   Nox
-const int SIGNAL_PIN3 = 4;  //  25   Fill
-const int SIGNAL_PIN4 = 6;  //  21   Vent
-const int SIGNAL_PIN5 = 13; //  26   ---
+const int SIGNAL_PIN1 = 4; //  23   ETh
+const int SIGNAL_PIN2 = 5; //  24   Nox
+const int SIGNAL_PIN3 = 2; //  25   Fill
+const int SIGNAL_PIN4 = 6; //  21   Vent    now 8
+const int SIGNAL_PIN5 = 3; //  26   ---
 
 // Pin to receive the signal from the first Arduino
 // Pin to control the buzzer
@@ -27,17 +27,13 @@ int num = 0;
 void setup()
 {
   Serial.begin(19200);
+  pinMode(LED_BUILTIN, OUTPUT);
   // make sure we pin up at the beginnning
-  pinMode(SIGNAL_PIN1, INPUT_PULLUP);
-  pinMode(SIGNAL_PIN2, INPUT_PULLUP);
-  pinMode(SIGNAL_PIN3, INPUT_PULLUP);
-  pinMode(SIGNAL_PIN4, INPUT_PULLUP);
-  pinMode(SIGNAL_PIN5, INPUT_PULLUP);
-
-  pinMode(N_PIN, INPUT_PULLUP);
-  pinMode(E_PIN, INPUT_PULLUP);
-  pinMode(F_PIN, INPUT_PULLUP);
-  pinMode(V_PIN, INPUT_PULLUP);
+  pinMode(SIGNAL_PIN1, INPUT);
+  pinMode(SIGNAL_PIN2, INPUT);
+  pinMode(SIGNAL_PIN3, INPUT);
+  pinMode(SIGNAL_PIN4, INPUT);
+  pinMode(SIGNAL_PIN5, INPUT);
 
   n.attach(N_PIN);
   e.attach(E_PIN);
@@ -64,10 +60,10 @@ void loop()
 
   if (digitalRead(SIGNAL_PIN2) == HIGH)
   {
-    n.write(OPEN);
+    n.write(120); // IREC change
   }
   else
-    n.write(CLOSE);
+    n.write(0);
 
   if (digitalRead(SIGNAL_PIN3) == HIGH)
   {
@@ -102,4 +98,3 @@ void loop()
   Serial.print(" ");
   Serial.println(digitalRead(SIGNAL_PIN5) == HIGH);
 }
-message
